4e365de49caa18c3e9108a7f4c10c89fdcef3f65
QpYujin <yujin@qpair.io>
2019-01-18 10:46:17 -0500

    Changed format

--- config.sh
+++ config.sh
@@ -1,4 +1,11 @@
+#! /bin/bash
+
 INSTANCE_COUNT=1
 INSTANCE_SIZE=t2.medium
 ELASTIC_IP=34.226.111.33
 REGION=us-east-1
+
+export INSTANCE_COUNT
+export INSTANCE_SIZE	
+export ELASTIC_IP	
+export REGION
